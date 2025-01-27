3b4fb03c58110bd4dc1bcacc0d7633bc2b814a18
Papugator <coffiein4ik@mail.ru>
2025-01-28 01:12:50 +0300

    Add files via upload

---
+++ Check1_codewars1.py
@@ -0,0 +1,5 @@
+'''Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python'''
+
+def opposite(number):
+    return -number
